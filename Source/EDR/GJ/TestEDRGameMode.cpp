b7dcdb2ecd88702e00acf40096bce7160816f06c
keeid <h1372469@gmail.com>
2024-06-27 19:20:34 +0900

    건주 필터 폴더 제작

---
+++ Source/EDR/GJ/TestEDRGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestEDRGameMode.h"
+#include "TestEDRCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATestEDRGameMode::ATestEDRGameMode()
+{
+	//// set default pawn class to our Blueprinted character
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
+}
