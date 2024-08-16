ca28137d1641ea7d29bd565fd4f0545d4bc1860e
이건주\leegu <lgj415415@naver.com>
2024-08-16 15:30:51 +0900

    적캐릭터 hp 50이하가 될경우 pase 2 출력

--- Source/EDR/GJ/MyCharacter.h
+++ Source/EDR/GJ/MyCharacter.h
@@ -24,12 +24,13 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	UPROPERTY(BluePrintReadWrite)
+	float hp = 100.0f;
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 	FORCEINLINE float GetHp(){return hp;}
 
 private:
-	UPROPERTY()
-	float hp = 100.0f;
+	
 };
