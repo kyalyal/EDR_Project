ca814896cd8ba5f6b9405c92c9cd60a1c7257882
junseungwon <70639975+junseungwon@users.noreply.github.com>
2024-09-19 17:57:03 +0900

    BTT 오류수정

--- Source/EDR/GJ/BTT_EDR_Attack.cpp
+++ Source/EDR/GJ/BTT_EDR_Attack.cpp
@@ -1,11 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
 #include "MyCharacter.h"
 #include "BTT_EDR_Attack.h"
 
 EBTNodeResult::Type UBTT_EDR_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-    Super::ExecuteTask(OwnerComp, NodeMemory);
    
 
    Monster = Cast<AMyCharacter>(OwnerComp.GetOwner());
