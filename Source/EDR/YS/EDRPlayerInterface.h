f25efb29d4fbf407f9b72419345d648efa6a1ebc
junseungwon <70639975+junseungwon@users.noreply.github.com>
2024-09-19 18:09:17 +0900

    승원이 NPC Action함수만들기

--- Source/EDR/YS/EDRPlayerInterface.h
+++ Source/EDR/YS/EDRPlayerInterface.h
@@ -23,6 +23,6 @@ class EDR_API IEDRPlayerInterface
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 
-	
+
 	virtual void PlayerInteract() = 0;
 };
