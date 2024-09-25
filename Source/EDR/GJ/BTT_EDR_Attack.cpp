5607e04023696b416a8e528efbb3daaa5c849e63
2sang123 <2sangkihu@naver.com>
2024-09-25 22:28:57 +0900

    Revert "Merge branch 'main' into LSK"
    This reverts commit 9d2f89496bfea4e6f4477a6a4c0b5385d2a19b47, reversing
    changes made to 4c5ebb47d079c004ccee18f7acb00cf7b4b7bd87.

--- Source/EDR/GJ/BTT_EDR_Attack.cpp
+++ Source/EDR/GJ/BTT_EDR_Attack.cpp
@@ -1,22 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BTT_EDR_Attack.h"
+
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/BlackboardData.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "Blueprint/AIBlueprintHelperLibrary.h"
 #include "MyCharacter.h"
-
+#include "BTT_EDR_Attack.h"
 
 EBTNodeResult::Type UBTT_EDR_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
    
-    
+
    Monster = Cast<AMyCharacter>(OwnerComp.GetOwner());
 
    if (IsValid(Monster))
    {
        
+
        return EBTNodeResult::Succeeded;
    }
 
