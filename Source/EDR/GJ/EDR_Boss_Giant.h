fa4e2d076e108826d03e028e1c0e76c75ad9b313
이건주\leegu <lgj415415@naver.com>
2024-10-13 09:41:53 +0900

    Giant c++파일 만듬

---
+++ Source/EDR/GJ/EDR_Boss_Giant.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyCharacter.h"
+#include "EDR_Boss_Giant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDR_API AEDR_Boss_Giant : public AMyCharacter
+{
+	GENERATED_BODY()
+	
+};
