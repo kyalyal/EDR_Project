4d6a930c2e613af2490275cfbed7c581356169d0
이건주\leegu <lgj415415@naver.com>
2024-10-22 12:24:06 +0900

    죽으면 FigthMode 꺼지게

--- Source/EDR/GJ/BTS_EDR_IsDeath.cpp
+++ Source/EDR/GJ/BTS_EDR_IsDeath.cpp
@@ -4,7 +4,7 @@
 #include "BehaviorTree/BlackboardComponent.h"
 #include "MyCharacter.h"
 #include "Enemy_EDR_AIController.h"
-
+#include "EDR/YS/EDRGameMode.h"
 UBTS_EDR_IsDeath::UBTS_EDR_IsDeath()
 {
 
@@ -12,6 +12,20 @@ UBTS_EDR_IsDeath::UBTS_EDR_IsDeath()
 void UBTS_EDR_IsDeath::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
     Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+    APawn* ControllingPawn = OwnerComp.GetAIOwner()->GetPawn();
+    UWorld* World = ControllingPawn->GetWorld();
+    if (nullptr == ControllingPawn)
+    {
+        return;
+    }
+    if (nullptr == World)
+    {
+        return;
+    }
+
+    // 게임모드 캐스팅
+    AEDRGameMode* GameMode = Cast<AEDRGameMode>(World->GetAuthGameMode());
+
 
     // AIController 가져오기
     AEnemy_EDR_AIController* AIController = Cast<AEnemy_EDR_AIController>(OwnerComp.GetOwner());
@@ -23,7 +37,18 @@ void UBTS_EDR_IsDeath::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMe
         {
             // Death 변수가 true인지 확인
             bIsDeath = MyCharacter->Death; // Death 변수가 public이어야 함
-            OwnerComp.GetBlackboardComponent()->SetValueAsBool(AEnemy_EDR_AIController::Death, bIsDeath);
+            if (bIsDeath)
+            {
+                OwnerComp.GetBlackboardComponent()->SetValueAsBool(AEnemy_EDR_AIController::Death, bIsDeath);
+                // 게임모드가 널이 아닐때
+                if (GameMode)
+                {
+                    // EFightMode FightMode로 설정
+                    GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT("FightModeOFF!!!!!!!!!!!!!!!!!!"));
+                    GameMode->SetFightMode(EFightMode::None);
+
+                }
+            }
         }
     }
 }
