6596fc587bef11882e7f4cff11579ca55ec28001
이건주\leegu <lgj415415@naver.com>
2024-10-28 18:03:10 +0900

    소켓 각도 조정

--- Source/EDR/GJ/EDR_Boss_Knight.cpp
+++ Source/EDR/GJ/EDR_Boss_Knight.cpp
@@ -15,24 +15,29 @@ AEDR_Boss_Knight::AEDR_Boss_Knight()
 	GetCapsuleComponent()->SetCapsuleRadius(34.0f);
 	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
 
+
+	// 메시 받아오기
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh>MODEL(TEXT("/Game/BLACKSUN_BOSSBUNDLE1/Characters/Thral/SK_ThrallRigged.SK_ThrallRigged"));
 	if (MODEL.Succeeded())
 	{
 		GetMesh()->SetSkeletalMesh(MODEL.Object);
 	}
+
+	// 크기 1.6배 키우기
 	SetActorScale3D(FVector(1.6f, 1.6f, 1.6f));
 }
 void AEDR_Boss_Knight::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// 게임 실행시 캐릭터 손에 무기 장착
 	FName WeaponSocket(TEXT("hand_rSocket"));
 	CurrentWeapon = GetWorld()->SpawnActor<AEDR_Enemy_Weapon>(FVector::ZeroVector, FRotator::ZeroRotator);
 	if (nullptr != CurrentWeapon)
 	{
 		CurrentWeapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, WeaponSocket);
-		CurrentWeapon->SetActorRelativeRotation(FRotator(0.0f, 0.0f, 180.0f)); // 180도 회전
-		CurrentWeapon->SetActorRelativeLocation(FVector(-10.0f, 5.0f, 56.0f)); // 무기이동
+		//CurrentWeapon->SetActorRelativeRotation(FRotator(0.0f, 0.0f, 180.0f)); // 180도 회전
+		CurrentWeapon->SetActorRelativeLocation(FVector(0.0f, 0.0f, -10.0f)); // 무기이동
 
 
 		// 무기 크기 키우기
